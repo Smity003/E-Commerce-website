17394571a575498968fbba820d64d7f7caf16c68
Smity003 <sayenkule@gmail.com>
2024-06-25 22:14:07 +0530

    first commit

---
+++ header.php
@@ -0,0 +1,233 @@
+<?php
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		 <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+		<title>CS20B045 DBMS</title>
+
+		<!-- Google font -->
+		<link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700" rel="stylesheet"/>
+
+		<!-- Bootstrap -->
+		<link type="text/css" rel="stylesheet" href="css/bootstrap.min.css"/>
+
+		<!-- Slick -->
+		<link type="text/css" rel="stylesheet" href="css/slick.css"/>
+		<link type="text/css" rel="stylesheet" href="css/slick-theme.css"/>
+
+		<!-- nouislider -->
+		<link type="text/css" rel="stylesheet" href="css/nouislider.min.css"/>
+
+		<!-- Font Awesome Icon -->
+		<link rel="stylesheet" href="css/font-awesome.min.css">
+
+		<!-- Custom stlylesheet -->
+		<link type="text/css" rel="stylesheet" href="css/style.css"/>
+		<link type="text/css" rel="stylesheet" href="css/accountbtn.css"/>
+	
+    </head>
+	<body>
+		<!-- HEADER -->
+		<header>
+			<!-- TOP HEADER -->
+			<div id="top-header">
+				<div class="container">
+					<ul class="header-links pull-left">
+						<li><a href="#"><i class="fa fa-phone"></i> +91-1234567890</a></li>
+						<li><a href="#"><i class="fa fa-envelope-o"></i> SmitYenkule@gmail.com</a></li>
+						<li><a href="#"><i class="fa fa-map-marker"></i>IIT T</a></li>
+					</ul>
+					<ul class="header-links pull-right">
+						<li><a href="#"><i class="fa fa-inr"></i> INR</a></li>
+						<li><?php
+						
+                             include "db.php";
+                            if(isset($_SESSION["uid"])){
+                                $sql = "SELECT first_name FROM user_info WHERE user_id='$_SESSION[uid]'";
+                                $query = mysqli_query($con,$sql);
+                                $row=mysqli_fetch_array($query);
+                                
+                                echo '
+                               <div class="dropdownn">
+                                  <a href="#" class="dropdownn" data-toggle="modal" data-target="#myModal" ><i class="fa fa-user-o"></i> HI '.$row["first_name"].'</a>
+								  <div class="dropdownn-content">
+								  	<a href="myorders.php"  ><i class="fa fa-shopping-basket" aria-hidden="true"></i>My Order</a>
+                                    <a href="" data-toggle="modal" data-target="#profile"><i class="fa fa-user-circle" aria-hidden="true" ></i>My Profile</a>
+                                    <a href="logout.php"  ><i class="fa fa-sign-in" aria-hidden="true"></i>Log out</a>
+                                    
+                                  </div>
+                                </div>';
+
+                            }else{ 
+                                echo '
+                                <div class="dropdownn">
+                                  <a href="#" class="dropdownn" data-toggle="modal" data-target="#myModal" ><i class="fa fa-user-o"></i> My Account</a>
+								  <div class="dropdownn-content">
+								  	<a href="admin/login.php" ><i class="fa fa-user" aria-hidden="true" ></i>Admin</a>
+                                    <a href="signin_form.php"><i class="fa fa-sign-in" aria-hidden="true" ></i>Login</a>
+                                    <a href="signup_form.php"><i class="fa fa-user-plus" aria-hidden="true"></i>Register</a>
+                                    
+                                  </div>
+                                </div>';
+                                
+                            }
+                                             ?>
+                               
+                                </li>				
+					</ul>
+					
+				</div>
+			</div>
+			<!-- /TOP HEADER -->
+			
+			
+
+			<!-- MAIN HEADER -->
+			<div id="header">
+				<!-- container -->
+				<div class="container">
+					<!-- row -->
+					<div class="row">
+						<!-- LOGO -->
+						<div class="col-md-3">
+							<div class="header-logo">
+								<a href="index.php" class="logo">
+								<font style="font-style:normal; font-size: 33px;font-family: serif">
+                                        E Commerce Website
+                                    </font>
+									
+								</a>
+							</div>
+						</div>
+						<!-- /LOGO -->
+
+						<!-- ACCOUNT -->
+						<div class="col-md-3 clearfix">
+							<div class="header-ctn">
+								<!-- Wishlist -->
+								
+								<!-- /Wishlist -->
+								<div>
+									<a href="wishlist.php">
+										<i class="fa fa-heart-o"></i>
+										<span>Your Wishlist</span>
+										<div id="wishlist-badge" class="qty">0</div>
+									</a>
+								</div>
+								<!-- Cart -->
+								<div class="dropdown">
+									<a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">
+										<i class="fa fa-shopping-cart"></i>
+										<span>Your Cart</span>
+										<div class="badge qty">0</div>
+									</a>
+									<div class="cart-dropdown"  >
+										<div class="cart-list" id="cart_product">
+										
+											
+										</div>
+										
+										<div class="cart-btns">
+												<a href="cart.php" style="width:100%;"><i class="fa fa-edit"></i>  edit cart</a>
+											
+										</div>
+									</div>
+										
+									</div>
+								<!-- /Cart -->
+
+								<!-- Menu Toogle -->
+								<div class="menu-toggle">
+									<a href="#">
+										<i class="fa fa-bars"></i>
+										<span>Menu</span>
+									</a>
+								</div>
+								<!-- /Menu Toogle -->
+							</div>
+						</div>
+						<!-- /ACCOUNT -->
+					</div>
+					<!-- row -->
+				</div>
+				<!-- container -->
+			</div>
+			<!-- /MAIN HEADER -->
+		</header>
+		<!-- /HEADER -->
+		<nav id='navigation'>
+			<!-- container -->
+			<div class="container" id="get_category_home">
+				 <div id="responsive-nav">
+						
+					<ul class="main-nav nav navbar-nav">
+						<li class="active"><a href="index.php">Home</a></li>
+						<li class="category" cid="1"><a href="store.php">Electronics</a></li>
+						<li class="category" cid="2"><a href="store.php">Ladies Wears</a></li>
+						<li class="category" cid="3"><a href="store.php">Mens Wear</a></li>
+						<li class="category" cid="4"><a href="store.php">Kids Wear</a></li>
+						<li class="category" cid="5"><a href="store.php">Furnitures</a></li>
+						<li class="category" cid="6"><a href="store.php">Home Appliances</a></li>
+						<li class="category" cid="7"><a href="store.php">Electronics Gadgets</a></li>
+					
+					</ul>
+						
+				</div> 
+			</div>
+			<!-- /container -->
+		</nav>
+            
+
+		<!-- NAVIGATION -->
+		
+		<div class="modal fade" id="Modal_login" role="dialog">
+                        <div class="modal-dialog">
+													
+                          <!-- Modal content-->
+                          <div class="modal-content">
+                            <div class="modal-header">
+                              <button type="button" class="close" data-dismiss="modal">&times;</button>
+                              
+                            </div>
+                            <div class="modal-body">
+                            <?php
+                                include "login_form.php";
+    
+                            ?>
+          
+                            </div>
+                            
+                          </div>
+													
+                        </div>
+                      </div>
+                <div class="modal fade" id="Modal_register" role="dialog">
+                        <div class="modal-dialog" style="">
+
+                          <!-- Modal content-->
+                          <div class="modal-content">
+                            <div class="modal-header">
+                              <button type="button" class="close" data-dismiss="modal">&times;</button>
+                              
+                            </div>
+                            <div class="modal-body">
+                            <?php
+                                include "register_form.php";
+    
+                            ?>
+          
+                            </div>
+                            
+                          </div>
+
+                        </div>
+                      </div>
+		
